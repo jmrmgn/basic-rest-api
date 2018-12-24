b0a53746cee96bec7623092f3d32e609ab60a5fd
Jomar <jomar.magno026@gmail.com>
2018-12-24 12:08:22 +0800

    setup headers

--- app.js
+++ app.js
@@ -8,6 +8,14 @@ const app = express();
 // application/json parser
 app.use(bodyParser.json());
 
+// setup Header to avoid CORS
+app.use((req, res, next) => {
+   res.setHeader('Access-Control-Allow-Origin', '*');
+   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+   next();
+});
+
 // Routes
 app.use('/feed', feedRoutes);
 
